9b7a0618f92acc61d21c1345ce33e1ad6072f269
alvaro <aaruizf@unal.edu.co>
2021-02-26 07:12:23 -0500

    Add some styling and tests for models

---
+++ app/controllers/opinions_controller.rb
@@ -0,0 +1,22 @@
+class OpinionsController <
+  before_action :authenticate_user!
+
+  def index
+    @opinions = current_user.opinions.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-  def index; end
+  # before_action :authenticate_user!
+  def index
+    @users = User.all
+  end
+
+  def edit
+  end
+
+  def show
+      @user = User.find(params[:id])
+      @opinions = @user.opinions.all
+  end
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  def homepage; end
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:fullname, :email, :password, :password_confirmation)
+  end
 
 end
